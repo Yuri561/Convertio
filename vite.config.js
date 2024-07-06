614c556cacca3373f694cb3666c43115acdcaa95
Yuri Pierre-Louis <hazelirvin@gmail.com>
2024-07-06 02:03:51 -0400

    added aos

--- vite.config.js
+++ vite.config.js
@@ -3,8 +3,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-    optimizeDeps: {
-    include: ['@ffmpeg/ffmpeg'],
-  },
-})
+	plugins: [react()],
+	optimizeDeps: {
+		include: ['@ffmpeg/ffmpeg'],
+	},
+	rollupOptions: {
+		external: ['aos'],
+	},
+});
